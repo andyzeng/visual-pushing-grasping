abf5e89000a3fe3bcd060d904dea7ca14198226d
Andy Zeng <andy.zeng.workhorse@gmail.com>
2018-04-16 00:46:10 -0700

    permissions update

--- downloads/download-weights.sh
+++ downloads/download-weights.sh
@@ -1,2 +1,3 @@
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 wget http://vision.princeton.edu/projects/2018/vpg/downloads/vpg-original-sim-pretrained-10-obj.pth
+wget http://vision.princeton.edu/projects/2018/vpg/downloads/vpg-original-real-pretrained-30-obj.pth
